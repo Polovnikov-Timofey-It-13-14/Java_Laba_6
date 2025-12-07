3c0bf96fbb10cb9c4315c818043631cd2540fc7e
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 16:47:35 +0500

    Update Cache.java

--- src/Cache.java
+++ src/Cache.java
@@ -1,10 +1,27 @@
+package annotations;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания кэшируемых свойств или методов.
+ *
+ * <p>Эта аннотация используется для пометки классов, указывая, какие свойства
+ * или методы должны быть кэшированы.</p>
+ *
+ *
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Cache {
+
+    /**
+     * Массив имен свойств или методов, которые должны быть кэшированы.
+     * <p>Определяет, какие именно элементы класса подлежат кэшированию. </p>
+     *
+     * @return массив имен свойств или методов для кэширования
+     */
     String[] value() default {};
 }
