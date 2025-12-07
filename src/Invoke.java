ad16e6adf204f51005a8a55d869ed24b56571d67
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 16:56:30 +0500

    Update Invoke.java

--- src/Invoke.java
+++ src/Invoke.java
@@ -1,8 +1,18 @@
+package annotations;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для пометки методов, которые должны быть автоматически вызваны.
+ *
+ * <p>Эта аннотация используется для указания методов, которые требуется
+ * выполнить автоматически.</p>
+ *
+ * @see java.lang.reflect.Method
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Invoke {
