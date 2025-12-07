7a8399e12cc24b1d3cfc5c61a97b7c1401b294b9
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 17:19:53 +0500

    Update Validate.java

--- src/Validate.java
+++ src/Validate.java
@@ -1,10 +1,25 @@
+package annotations;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания классов валидаторов, которые должны быть применены.
+ *
+ * <p>Эта аннотация используется для пометки классов или других аннотаций.</p>
+ *
+ * @see java.lang.annotation.Annotation
+ */
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Validate {
+
+    /**
+     * Массив классов валидаторов, которые должны быть применены.</p>
+     *
+     * @return массив классов валидаторов
+     */
     Class<?>[] value();
 }
