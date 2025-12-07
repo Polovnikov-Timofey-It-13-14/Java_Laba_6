7df7840e833784df16d2cc07cc100704e8f5be11
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 16:50:49 +0500

    Update Default.java

--- src/Default.java
+++ src/Default.java
@@ -1,10 +1,27 @@
+package annotations;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания класса по умолчанию или значения по умолчанию.
+ *
+ * <p>Эта аннотация позволяет указать класс по умолчанию, который должен быть использован
+ *  по умолчанию для полей или типов.</p>
+ *
+ * @since 2024
+ */
 @Target({ElementType.TYPE, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Default {
+
+    /**
+     * Класс по умолчанию, который должен быть использован.
+     * <p>Определяет класс, который будет служить реализацией по умолчанию </p>
+     *
+     * @return класс по умолчанию
+     */
     Class<?> value();
 }
