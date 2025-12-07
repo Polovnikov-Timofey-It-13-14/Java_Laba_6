355373fd1dc36a3ba28e4601308bb99d19308fe6
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 17:13:14 +0500

    Update TwoAnnotation.java

--- src/TwoAnnotation.java
+++ src/TwoAnnotation.java
@@ -1,6 +1,27 @@
+package handlers;
+
+import annotations.Two;
+
+/**
+ * Обработчик аннотации {@link annotations.Two}.
+ * <p>Предоставляет методы для работы с классами, помеченными аннотацией {@code @Two},
+ * включая чтение значений и валидацию.</p>
+ *
+ * @see annotations.Two
+ */
 public class TwoAnnotation {
 
-    //Читает и выводит значения аннотации @Two
+    /**
+     * Читает и выводит значения аннотации {@link annotations.Two}.
+     * <p>Если класс помечен аннотацией {@code @Two}, выводит значения
+     * её свойств {@code first} и {@code second} в консоль.</p>
+     *
+     * @param classes класс для проверки на наличие аннотации {@code @Two}
+     * @throws NullPointerException если переданный класс равен {@code null}
+     *
+     * @see annotations.Two#first()
+     * @see annotations.Two#second()
+     */
     public static void processAnnotation(Class<?> classes) {
         if (classes.isAnnotationPresent(Two.class)) {
             Two annotation = classes.getAnnotation(Two.class);
@@ -10,7 +31,15 @@ public class TwoAnnotation {
         }
     }
 
-    //Валидирует аннотацию @Two и выбрасывает исключение при некорректных значениях
+    /**
+     * Валидирует аннотацию {@link annotations.Two} и выбрасывает исключение при некорректных значениях.
+     *
+     * @param classes класс для валидации аннотации {@code @Two}
+     * @throws IllegalArgumentException если:
+     *
+     * @see annotations.Two#first()
+     * @see annotations.Two#second()
+     */
     public static void validateTwoAnnotation(Class<?> classes) {
         if (!classes.isAnnotationPresent(Two.class)) {
             throw new IllegalArgumentException("Класс " + classes.getSimpleName() + " не содержит аннотацию @Two");
