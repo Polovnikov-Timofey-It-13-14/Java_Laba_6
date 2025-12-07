55ee9df3d04be12d607fe0569003e91c07d8b174
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 17:11:41 +0500

    Update Two.java

--- src/Two.java
+++ src/Two.java
@@ -1,11 +1,37 @@
+package annotations;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация с двумя обязательными свойствами.
+ *
+ * <p>Эта аннотация предназначена для пометки классов и содержит
+ * два обязательных параметра: строковый и числовой.</p>
+ *
+ * @see java.lang.annotation.Annotation
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Two {
+
+    /**
+     * Строковое свойство аннотации.
+     * <p>Это обязательное свойство, которое должно быть указано
+     * при использовании аннотации {@code @Two}.</p>
+     *
+     * @return строковое значение свойства
+     */
     String first();
+
+    /**
+     * Числовое свойство аннотации.
+     * <p>Это обязательное свойство, которое должно быть указано
+     * при использовании аннотации {@code @Two}.</p>
+     *
+     * @return целочисленное значение свойства
+     */
     int second();
 }
