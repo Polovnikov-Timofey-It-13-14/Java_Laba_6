598d863a2fe1f00b3787c2638defc42d39cd7dee
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 16:52:52 +0500

    Update DefaultAnnotation.java

--- src/DefaultAnnotation.java
+++ src/DefaultAnnotation.java
@@ -1,15 +1,48 @@
+package handlers;
+
+import annotations.Default;
+import model.Student;
+
+/**
+ * <p>Класс помечен аннотацией {@code @Default(Student.class)}, что указывает,
+ * что {@link model.Student} является классом по умолчанию для данного типа.</p>
+ *
+ * @see annotations.Default
+ * @see model.Student
+ */
 @Default(Student.class)
 public class DefaultAnnotation {
+
+    /** Факультет, к которому относится обработчик. */
     private String faculty;
 
+    /**
+     * Создает новый экземпляр {@code DefaultAnnotation} с факультетом
+     * "Computer Science" по умолчанию.
+     */
     public DefaultAnnotation() {
         this.faculty = "Computer Science";
     }
 
+    /**
+     * Возвращает название факультета, связанного с этим обработчиком.
+     *
+     * @return название факультета
+     */
     public String getDepartment() {
         return faculty;
     }
 
+    /**
+     * Обрабатывает переданного студента, выводя информацию о нем
+     * вместе с факультетом обработчика.
+     *
+     * <p>Выводит в консоль информацию о студенте в формате:
+     * "Обработка студента: [имя], курс: [курс], факультет: [факультет]"</p>
+     *
+     * @param student студент для обработки
+     * @throws NullPointerException если переданный студент равен {@code null}
+     */
     public void processStudent(Student student) {
         System.out.println("Обработка студента: " + student.getName() +
                 ", курс: " + student.getCourse() +
