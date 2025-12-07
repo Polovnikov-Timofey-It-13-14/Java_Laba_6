62dcb7b01c03c3fff17a4641f4e37a201a8ddad9
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 17:16:02 +0500

    Update TwoFirst.java

--- src/TwoFirst.java
+++ src/TwoFirst.java
@@ -1,11 +1,38 @@
-//Класс с некорректной аннотацией @Two - пустая строка в first
+package service;
+
+import annotations.Two;
+
+/**
+ * Класс с некорректной аннотацией {@link annotations.Two}.
+ * <p>Используется для тестирования обработки ошибок валидации аннотации {@code @Two}.
+ * Содержит аннотацию с пустой строкой в свойстве {@code first}, что является
+ * некорректным значением.</p>
+ *
+ * <p>Аннотация содержит:
+ * <ul>
+ *   <li>{@code first = ""} - пустая строка (некорректное значение)</li>
+ *   <li>{@code second = 123} - валидное положительное число</li>
+ * </ul>
+ * </p>
+ *
+ * @see annotations.Two
+ * @see handlers.TwoAnnotation#validateTwoAnnotation(Class)
+ */
 @Two(first = "", second = 123)  // first - пустая строка, second - валидный
 public class TwoFirst {
 
+    /**
+     * Создает экземпляр класса {@code TwoFirst}.
+     */
     public TwoFirst() {
         // Пустой конструктор
     }
 
+    /**
+     * Возвращает строковое представление объекта.
+     *
+     * @return строку "InvalidFirstClass{}"
+     */
     @Override
     public String toString() {
         return "InvalidFirstClass{}";
